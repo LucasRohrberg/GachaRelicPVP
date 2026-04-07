559d68cdc84d98c8219416fd5548c7aeb799fed3
LucasRohrberg <44026433+LucasRohrberg@users.noreply.github.com>
2026-04-07 17:41:37 +0200

    feat: genshin player search

--- gacharelicpvp/src/App.tsx
+++ gacharelicpvp/src/App.tsx
@@ -1,19 +1,82 @@
+import { useState } from "react";
 import "./App.css";
 
-const uid = "706312466";
-const res = await fetch(`http://localhost:4000/api/user/${uid}`);
-const user = await res.json();
-console.log(user);
-
 function App() {
+  const [player1, setPlayer1] = useState("708629925");
+  const [player2, setPlayer2] = useState("706312466");
+  const [game1, setGame1] = useState("");
+  const [game2, setGame2] = useState("");
+
+  // 708629925
+  // 706312466
+
   return (
     <>
       <div className="layout">
-        <div className="playerOne">
-          Player 1<input type="text"></input>
+        <p>Quick access: 708629925 706312466</p>
+        <div className="player-one">
+          Player 1
+          <input
+            type="text"
+            value={player1}
+            onChange={(e) => setPlayer1(e.target.value)}
+          />
+          <select value={game1} onChange={(e) => setGame1(e.target.value)}>
+            <option value="">Select Game</option>
+            <option value="genshin">Genshin Impact</option>
+            <option value="hsr" disabled>
+              Coming soon: Honkai: Star Rail
+            </option>
+            <option value="zzz" disabled>
+              Coming soon: Zenless Zone Zero
+            </option>
+          </select>
         </div>
-        <div className="playerTwo">
-          Player 2<input type="text"></input>
+        <div className="player-two">
+          Player 2
+          <input
+            type="text"
+            value={player2}
+            onChange={(e) => setPlayer2(e.target.value)}
+          />
+          <select value={game2} onChange={(e) => setGame2(e.target.value)}>
+            <option value="">Select Game</option>
+            <option value="genshin">Genshin Impact</option>
+            <option value="hsr" disabled>
+              Coming soon: Honkai: Star Rail
+            </option>
+            <option value="zzz" disabled>
+              Coming soon: Zenless Zone Zero
+            </option>
+          </select>
+        </div>
+        <button
+          onClick={async () => {
+            for (let i = 0; i < 2; i++) {
+              const res = await fetch(
+                `http://localhost:4000/api/user/${i === 0 ? player1 : player2}`,
+              );
+              const user = await res.json();
+
+              const list = document.querySelector(
+                i === 0 ? ".player-one-list" : ".player-two-list",
+              ) as HTMLDivElement;
+
+              list.innerHTML = "";
+
+              const characterList = user.characters;
+              for (let i = 0; i < characterList.length; i++) {
+                list.innerHTML += `
+                <img src="${user.characters[i].characterData.cardIcon.url}" alt="Character Icon" />`;
+              }
+            }
+          }}
+        >
+          Search
+        </button>
+        <div className="character-list">
+          <div className="player-one-list"></div>
+          <div className="player-two-list"></div>
         </div>
       </div>
     </>
